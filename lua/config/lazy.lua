36fcdc8acf0312036d8c8aca5c49a48c13972652
r8bywork <vanya.smolensky@gmail.com>
2023-11-28 20:59:03 +0300

    some fixes

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -13,7 +13,6 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   spec = {
-    -- add LazyVim and import its plugins
     {
       "LazyVim/LazyVim",
       import = "lazyvim.plugins",
@@ -25,7 +24,6 @@ require("lazy").setup({
         },
       },
     },
-    -- import any extras modules here
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
@@ -36,10 +34,7 @@ require("lazy").setup({
   },
   defaults = {
     lazy = false,
-    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-    -- have outdated releases, which may break your Neovim install.
     version = false, -- always use the latest git commit
-    -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   dev = {
     path = "~/.ghq/github.com",
@@ -48,14 +43,10 @@ require("lazy").setup({
   performance = {
     cache = {
       enabled = true,
-      -- disable_events = {},
     },
     rtp = {
-      -- disable some rtp plugins
       disabled_plugins = {
         "gzip",
-        -- "matchit",
-        -- "matchparen",
         "netrwPlugin",
         "rplugin",
         "tarPlugin",
--- lua/config/util.lua
+++ lua/config/util.lua
@@ -17,40 +17,4 @@ function M.toggle_colorcolumn()
   vim.notify("colorcolumn" .. " set to " .. tostring(value))
 end
 
--- https://github.com/folke/dot/blob/1007fc65738ad1f7a3e9c91432430017a6878378/nvim/lua/util/init.lua#L125
-function M.cowboy()
-  ---@type table?
-  local id
-  local ok = true
-  for _, key in ipairs({ "h", "j", "k", "l", "+", "-" }) do
-    local count = 0
-    local timer = assert(vim.loop.new_timer())
-    local map = key
-    vim.keymap.set("n", key, function()
-      if vim.v.count > 0 then
-        count = 0
-      end
-      if count >= 10 and vim.bo.buftype ~= "nofile" then
-        ok, id = pcall(vim.notify, "Whoa there, Cowboy!", vim.log.levels.WARN, {
-          icon = "🤠",
-          replace = id,
-          keep = function()
-            return count >= 10
-          end,
-        })
-        if not ok then
-          id = nil
-          return map
-        end
-      else
-        count = count + 1
-        timer:start(2000, 0, function()
-          count = 0
-        end)
-        return map
-      end
-    end, { expr = true, silent = true })
-  end
-end
-
 return M
