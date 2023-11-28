c996f02b5fe6e4726cd9fb8c02652d64907f6c64
r8bywork <vanya.smolensky@gmail.com>
2023-11-29 00:35:05 +0300

    add move between workspaces

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -45,6 +45,11 @@ vim.keymap.set("v", "<leader>cs", ":'<,'>sort<cr>", { desc = "Sort selected line
 -- Split window
 vim.keymap.set("n", "ss", ":split<Return>", opts)
 vim.keymap.set("n", "sv", ":vsplit<Return>", opts)
+-- Move window
+vim.keymap.set("n", "sh", "<C-w>h")
+vim.keymap.set("n", "sk", "<C-w>k")
+vim.keymap.set("n", "sj", "<C-w>j")
+vim.keymap.set("n", "sl", "<C-w>l")
 
 -- Tab
 vim.keymap.set("n", "te", ":tabedit")
